a99b1df5b31c47e4b6ec9d23ec2b0976ff492681
Raghu Vir <raghuvir@Raghus-MacBook-Pro.local>
2023-02-13 13:40:04 -0600

    Basic GoWebserver client

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	fileServer := http.FileServer(http.Dir("./static")) //we want a static directory --> look at index.html file
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 Not Found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error("Method is not supported", http.StatusNotFound)
+	}
+
+	fmt.Fprintf(w, "Hello!")
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST Request Successful!")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name: %s\n", name)
+	fmt.Fprintf(w, "Address: %s\n", address)
+}
